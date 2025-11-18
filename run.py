d7b566106727f5f52c4508a2b39f4c6858276d18
jackszb <55304530+jackszb@users.noreply.github.com>
2025-11-18 12:31:35 +0800

    Update run.py

--- run.py
+++ run.py
@@ -10,9 +10,7 @@ FILTER_URLS = [
     ("ppfeufer", "https://raw.githubusercontent.com/ppfeufer/adguard-filter-list/master/blocklist"),
     ("myadlist", "https://raw.githubusercontent.com/jackszb/MyAdList/main/dnsblock.txt"),
     ("217heidai", "https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdns.txt"),
-    ("ascopes", "https://ascopes.github.io/dns-blocklist/blocklist.txt"),
     ("mullvad", "https://raw.githubusercontent.com/cogwheel0/mullvad-to-adguard/main/adguard/all/adblock.txt"),
-    ("dns-blocklist", "https://raw.githubusercontent.com/jackszb/dns-blocklist/master/merged-dns-blocklist.txt"),
 ]
 
 OUTPUT_FILE = "merged_rules.txt"
